256e3b5c6fb77c209d2b9974e9c3f21e23a34437
Mohamed Selim <dev.m.selim@gmail.com>
2023-06-23 20:17:52 +0300

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'cars/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :cars, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/", to: "cars#index"
+  get "/details", to: "details#show"
+
 end
